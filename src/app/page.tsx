f39e5f2fbe7664516d758b8ac6cdeb4030babed5
JBryskiewicz <kuba@fiku.eu>
2023-06-28 15:43:49 +0200

    hero section on homepage with styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,25 @@ import {Header} from "@/components/Header";
 export default function Home() {
     return (
         <div>
-            <main>
+            <main className="hero">
                 <Header/>
-                <section></section>
+                <section className="flex flex-col items-end justify-center mx-32 hero-section">
+                    <div className="flex flex-col items-center justify-center pr-64 pb-28">
+                        <div className="flex flex-col items-center mb-6">
+                            <h1 className="text-4xl">Zacznij pomagać!</h1>
+                            <p className="text-4xl">Oddaj niechciane rzeczy w zaufane ręce</p>
+                        </div>
+                        <img
+                            src="../public/images/decoration-2x.png"
+                            alt="decoration-image"
+                            className="mb-12"
+                        />
+                        <div className="flex gap-4">
+                            <button className="btn btn-hero">ODDAJ RZECZY</button>
+                            <button className="btn btn-hero">ZORGANIZUJ ZBIÓRKĘ</button>
+                        </div>
+                    </div>
+                </section>
             </main>
         </div>
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,25 @@
 export function Header() {
     return (
         <header className="mx-32">
-            <div className="flex justify-end my-4 gap-8">
-                <button className="btn-header login">Zaloguj</button>
-                <button className="btn-header login">Załóż konto</button>
+            <div className="flex justify-end py-4 gap-8">
+                <button className="btn login">Zaloguj</button>
+                <button className="btn login">Załóż konto</button>
             </div>
             <nav>
                 <ul className="flex justify-end gap-8 text-lg">
-                    <li className="btn-header navigational">
+                    <li className="btn navigational">
                         <a href={"#"}>Start</a>
                     </li>
-                    <li className="btn-header navigational">
+                    <li className="btn navigational">
                         <a href={"#"}>O co chodzi?</a>
                     </li>
-                    <li className="btn-header navigational">
+                    <li className="btn navigational">
                         <a href={"#"}>O nas</a>
                     </li>
-                    <li className="btn-header navigational">
+                    <li className="btn navigational">
                         <a href={"#"}>Fundacje i organizacje</a>
                     </li>
-                    <li className="btn-header navigational">
+                    <li className="btn navigational">
                         <a href={"#"}>Kontakt</a>
                     </li>
                 </ul>
