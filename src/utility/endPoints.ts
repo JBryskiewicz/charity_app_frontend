629c17e5e8c605fe0d6a712812eabe7333c8e720
JBryskiewicz <kuba@fiku.eu>
2023-07-10 14:09:16 +0200

    Firebase initialized

--- src/utility/endPoints.ts
+++ src/utility/endPoints.ts
@@ -1,3 +1,1 @@
 export const CONTACT_MESSAGE_URL = 'https://fer-api.coderslab.pl/v1/portfolio/contact';
-
-export const FIRESTORE_DONATIONS = 'https://firestore.googleapis.com/v1/projects/charity-app-frontend/databases/donations';
